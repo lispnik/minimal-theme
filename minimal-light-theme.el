9cce5092157857eee07123bcc00cec508af6de7d
Matthew Kennedy <burnsidemk@gmail.com>
2015-08-03 16:47:07 -0500

    Customize sh-quoted-exec

--- minimal-light-theme.el
+++ minimal-light-theme.el
@@ -168,6 +168,9 @@
      `(js2-magic-paren ((,class (:underline t))))
      `(js2-private-function-call ((,class (:inherit base-faces))))
      `(js2-private-member ((,class (:inherit base-faces))))
+
+     ;; sh-mode
+     `(sh-quoted-exec ((,class (:underline t))))
    ))
 
 ;;;###autoload
